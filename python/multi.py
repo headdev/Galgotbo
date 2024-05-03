b4031fafbc89d198583e00825c74a93535e1c1b3
CFW HeadDev <31333238+headdev@users.noreply.github.com>
2024-05-03 05:04:45 -0500

    update paths
    update paths

--- python/multi.py
+++ python/multi.py
@@ -1,17 +1,20 @@
 import math
 import multiprocessing
-
 from web3 import Web3
 from typing import Dict
 from multicall import Call, Multicall
-
-from pools import Pool
+from poolsv3 import Poolv3
 from constants import logger
+from web3.middleware import geth_poa_middleware
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests
 
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-def get_uniswap_v2_reserves(https_url: str, pools: Dict[str, Pool]):
-    w3 = Web3(Web3.HTTPProvider(https_url))
-    signature = 'getReserves()((uint112,uint112,uint32))'  # reserve0, reserve1, blockTimestampLast
+def get_uniswap_v3_slot0(https_url: str, pools: Dict[str, Poolv3]):
+    w3 = Web3(Web3.HTTPProvider(https_url, request_kwargs={'verify': False}))
+    w3.middleware_onion.inject(geth_poa_middleware, layer=0)
+    signature = 'slot0()((uint160,int24,uint16,uint16,uint16,uint8,bool))' # sqrtPriceX96, tick, observationIndex, observationCardinality, observationCardinalityNext, feeProtocol, unlocked
 
     calls = []
     for pool_address in pools:
@@ -24,84 +27,71 @@ def get_uniswap_v2_reserves(https_url: str, pools: Dict[str, Pool]):
 
     multicall = Multicall(calls, _w3=w3)
     result = multicall()
-    reserves = {k: list(v)[:2] for k, v in result.items()}
+
+    slot0 = {k: v[0] for k, v in result.items()}
+
     """
-    reserves:
+    slot0:
     {
-        '0xF4b8A02D4e8D76070bD7092B54D2cBbe90fa72e9': [17368643486106939361172, 31867695075486],
-        '0x80067013d7F7aF4e86b3890489AcAFe79F31a4Cb': [5033262526671305584632, 9254792586342]
+        '0x8ad599c3A0ff1De082011EFDDc58f1908eb6e6D8': (1526350623543413024640558359979021026715832147638, 256819, 0, 1, 1, 0, True),
+        '0x88e6A0c2dDD26FEEb64F039a2c41296FcB3f5640': (1523585533016435498691267815985649526319479687826, 256232, 0, 1, 1, 0, True)
     }
     """
-    return reserves
 
+    return slot0
 
-def batch_get_uniswap_v2_reserves(https_url: str, pools: Dict[str, Pool]):
+def batch_get_uniswap_v3_slot0(https_url: str, pools: Dict[str, Poolv3]):
     mp = multiprocessing.Pool()
-    
     pools_cnt = len(pools)
-    batch = math.ceil(pools_cnt / 250)
+    batch = max(1, math.ceil(pools_cnt / 250))  # Ensure batch is at least 1
     pools_per_batch = math.ceil(pools_cnt / batch)
-    
+
     args = []
-    
     for i in range(batch):
         start_idx = i * pools_per_batch
         end_idx = min(start_idx + pools_per_batch, pools_cnt)
-        args.append((https_url, list(pools.keys())[start_idx:end_idx]))
-        
-    results = mp.starmap(get_uniswap_v2_reserves, args)
-    
-    reserves = {}
-    for result in results:
-        reserves = {**reserves, **result}
-        
-    return reserves
+        args.append((https_url, dict(list(pools.items())[start_idx:end_idx])))
 
+    results = mp.starmap(get_uniswap_v3_slot0, args)
+
+    slot0 = {}
+    for result in results:
+        slot0 = {**slot0, **result}
+
+    return slot0
 
 if __name__ == '__main__':
     import os
     import time
     from dotenv import load_dotenv
-
-    from pools import load_all_pools_from_v2
-    from paths import generate_triangular_paths
+    from poolsv3 import load_all_pools_from_v3
+    from pathsv3 import generate_triangular_paths
 
     load_dotenv(override=True)
-
     HTTPS_URL = os.getenv('HTTPS_URL')
 
     # Example on Ethereum
-    uniswap_v2_factory_addresses = ['0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac']
-    uniswap_v2_factory_blocks = [10794229]
+    factory_addresses = ['0x1F98431c8aD98523631AE4a59f267346ea31F984']
+    factory_blocks = [55859483]
 
-    w3 = Web3(Web3.HTTPProvider(HTTPS_URL))
-    pools = load_all_pools_from_v2(HTTPS_URL, uniswap_v2_factory_addresses, uniswap_v2_factory_blocks, 50000)
+    pools = load_all_pools_from_v3(HTTPS_URL, factory_addresses, factory_blocks, 1000)
     logger.info(f'Pool count: {len(pools)}')
-    
-    usdt_address = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
-    paths = generate_triangular_paths(pools, usdt_address)
-    
+
+    weth_address = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    paths = generate_triangular_paths(pools, weth_address)
+
     # Filter pools that were used in arb paths
     pools = {}
     for path in paths:
         pools[path.pool_1.address] = path.pool_1
         pools[path.pool_2.address] = path.pool_2
         pools[path.pool_3.address] = path.pool_3
-        
+
     logger.info(f'New pool count: {len(pools)}')
 
-    """
-    It seems like passing in thousands of pools as input halts to a stop somehow,
-    or it simply takes too long to retrieve data.
-    
-    Thus, filter out the pools you're going to use in arb. paths,
-    and batch request those pools.
-    
-    Benchmark: requesting 929 pools takes 3 seconds
-    """
     s = time.time()
-    reserves = batch_get_uniswap_v2_reserves(HTTPS_URL, pools)
+    slot0 = batch_get_uniswap_v3_slot0(HTTPS_URL, pools)
     e = time.time()
     logger.info(f'Took: {e - s} seconds')
-    logger.info(len(reserves))
-    logger.info(reserves)
+    logger.info(len(slot0))
+    logger.info(slot0)
--- python/paths.py
+++ python/paths.py
@@ -1,64 +1,77 @@
 from tqdm import tqdm
 from typing import Dict, List, Optional
 
-from pools import Pool
+from poolsv3 import Poolv3
 from bundler import Path
 from constants import logger
-from simulator import UniswapV2Simulator
+from simulatorv3 import UniswapV3Simulator
 
 
 class ArbPath:
-    
+
     def __init__(self,
-                 pool_1: Pool,
-                 pool_2: Pool,
-                 pool_3: Optional[Pool],
-                 zero_for_one_1: bool,
-                 zero_for_one_2: bool,
-                 zero_for_one_3: bool):
-        
+                 pool_1: Poolv3,
+                 pool_2: Poolv3,
+                 pool_3: Optional[Poolv3],
+                 token_in_1: str,
+                 token_out_1: str,
+                 token_in_2: str,
+                 token_out_2: str,
+                 token_in_3: str,
+                 token_out_3: str,
+                 fee_1: int,
+                 fee_2: int,
+                 fee_3: int):
+
         self.pool_1 = pool_1
         self.pool_2 = pool_2
         self.pool_3 = pool_3
-        self.zero_for_one_1 = zero_for_one_1
-        self.zero_for_one_2 = zero_for_one_2
-        self.zero_for_one_3 = zero_for_one_3
-        
+        self.token_in_1 = token_in_1
+        self.token_out_1 = token_out_1
+        self.token_in_2 = token_in_2
+        self.token_out_2 = token_out_2
+        self.token_in_3 = token_in_3
+        self.token_out_3 = token_out_3
+        self.fee_1 = fee_1
+        self.fee_2 = fee_2
+        self.fee_3 = fee_3
+
     @property
     def nhop(self) -> int:
         return 2 if self.pool_3 is None else 3
-    
+
     def has_pool(self, pool: str) -> bool:
         is_pool_1 = self.pool_1.address == pool
         is_pool_2 = self.pool_2.address == pool
         is_pool_3 = self.pool_3.address == pool
         return is_pool_1 or is_pool_2 or is_pool_3
-    
+
     def should_blacklist(self, blacklist_tokens: List[str]) -> bool:
         for i in range(self.nhop):
-            pool = getattr(self, f'pool_{i + 1}')
-            if pool.token0 in blacklist_tokens or pool.token1 in blacklist_tokens:
+            token_in = getattr(self, f'token_in_{i + 1}')
+            token_out = getattr(self, f'token_out_{i + 1}')
+            if token_in in blacklist_tokens or token_out in blacklist_tokens:
                 return True
         return False
-    
-    def simulate_v2_path(self, amount_in: float, reserves: Dict[str, Pool]):
+
+    def simulate_v3_path(self, amount_in: int, sqrtPriceX96: Dict[str, int]) -> int:
         """
-        Only works for v2 pool only paths
+        Simulates the swap path for Uniswap V3 pools
         """
-        token_in_decimals = self.pool_1.decimals0 if self.zero_for_one_1 else self.pool_1.decimals1
+        token_in_decimals = self.pool_1.decimals0 if self.token_in_1 == self.pool_1.token0 else self.pool_1.decimals1
         real_amount_in = int(amount_in * (10 ** token_in_decimals))
-        return simulate_v2_path(self, real_amount_in, reserves)
-    
+        return simulate_v3_path(self, real_amount_in, sqrtPriceX96)
+
     def optimize_amount_in(self,
                            max_amount_in: int,
                            step_size: int,
-                           reserves: Dict[str, Pool]) -> (int, int):
+                           sqrtPriceX96: Dict[str, int]) -> (int, int):
         # a simple brute force profit optimization
-        token_in_decimals = self.pool_1.decimals0 if self.zero_for_one_1 else self.pool_1.decimals1
+        token_in_decimals = self.pool_1.decimals0 if self.token_in_1 == self.pool_1.token0 else self.pool_1.decimals1
         optimized_in = 0
         profit = 0
         for amount_in in range(0, max_amount_in, step_size):
-            amount_out = self.simulate_v2_path(amount_in, reserves)
+            amount_out = self.simulate_v3_path(amount_in, sqrtPriceX96)
             this_profit = amount_out - (amount_in * (10 ** token_in_decimals))
             if this_profit >= profit:
                 optimized_in = amount_in
@@ -66,126 +79,132 @@ class ArbPath:
             else:
                 break
         return optimized_in, profit / (10 ** token_in_decimals)
-    
+
     def to_path_params(self, routers: List[str]) -> List[Path]:
         path_params = []
         for i in range(self.nhop):
             pool = getattr(self, f'pool_{i + 1}')
-            zero_for_one = getattr(self, f'zero_for_one_{i + 1}')
-            if zero_for_one:
-                token_in, token_out = pool.token0, pool.token1
-            else:
-                token_in, token_out = pool.token1, pool.token0
+            token_in = getattr(self, f'token_in_{i + 1}')
+            token_out = getattr(self, f'token_out_{i + 1}')
+            fee = getattr(self, f'fee_{i + 1}')
             path = Path(routers[i], token_in, token_out)
+            path.fee = fee
             path_params.append(path)
         return path_params
-    
-    
-def simulate_v2_path(path: ArbPath, amount_in: int, reserves: Dict[str, Pool]) -> int:
-    sim = UniswapV2Simulator()
-    
+
+
+def simulate_v3_path(path: ArbPath, amount_in: int, sqrtPriceX96: Dict[str, int]) -> int:
+    sim = UniswapV3Simulator()
+
     for i in range(path.nhop):
         pool = getattr(path, f'pool_{i + 1}')
-        zero_for_one = getattr(path, f'zero_for_one_{i + 1}')
-        reserve0 = reserves[pool.address][0]
-        reserve1 = reserves[pool.address][1]
-        fee = pool.fee
-        if zero_for_one:
-            reserve_in, reserve_out = reserve0, reserve1
-        else:
-            reserve_in, reserve_out = reserve1, reserve0
-        amount_out = sim.get_amount_out(amount_in, reserve_in, reserve_out, fee)
+        token_in = getattr(path, f'token_in_{i + 1}')
+        token_out = getattr(path, f'token_out_{i + 1}')
+        fee = getattr(path, f'fee_{i + 1}')
+        sqrt_ratio_current_x96 = sqrtPriceX96[pool.address]
+        sqrt_ratio_target_x96 = sim.sqrtx96_to_price(sqrt_ratio_current_x96,
+                                                     pool.decimals0,
+                                                     pool.decimals1,
+                                                     token_in == pool.token0)
+        liquidity = pool.liquidity
+        amount_out = sim.get_amount_out(amount_in, sqrt_ratio_current_x96, sqrt_ratio_target_x96, liquidity, fee)
         amount_in = amount_out
-        
+
     return amount_out
 
 
-def generate_triangular_paths(pools: Dict[str, Pool], token_in: str) -> List[ArbPath]:
+def generate_triangular_paths(pools: Dict[str, Poolv3], token_in: str) -> List[ArbPath]:
     """
-    A straightforward triangular arbitrage path finder.
+    A straightforward triangular arbitrage path finder for Uniswap V3.
     This call will find both 2-hop paths, 3-hop paths, but not more.
     Also, we define triangular arb. paths as a 3-hop swap path starting
     with token_in and ending with token_in:
-    
+
     token_in --> token1 --> token2 --> token_in
-    
+
     NOTE: this function is highly recursive, and can easily extend to n-hop paths.
     Refer to https://github.com/solidquant/whack-a-mole/blob/main/data/dex.py
     __generate_paths function for this.
     """
     paths = []
-    
+
     pools = list(pools.values())
-    
+
     for pool_1 in tqdm(pools,
                        total=len(pools),
                        ncols=100,
                        desc=f'Generating paths',
                        ascii=' =',
                        leave=True):
-        pools_in_path = []
-        pools_in_path.append(pool_1.address)
+        pools_in_path = set()
+        pools_in_path.add(pool_1.address)
         can_trade_1 = (pool_1.token0 == token_in) or (pool_1.token1 == token_in)
         if can_trade_1:
-            zero_for_one_1 = pool_1.token0 == token_in
-            (token_in_1, token_out_1) = (pool_1.token0, pool_1.token1) if zero_for_one_1 else (pool_1.token1, pool_1.token0)
+            token_in_1, token_out_1 = (pool_1.token0, pool_1.token1) if pool_1.token0 == token_in else (pool_1.token1, pool_1.token0)
+            fee_1 = pool_1.fee
             if token_in_1 != token_in:
                 continue
-            
+
             for j in range(len(pools)):
                 pool_2 = pools[j]
-                pools_in_path.append(pool_2.address)
+                pools_in_path.add(pool_2.address)
                 can_trade_2 = (pool_2.token0 == token_out_1) or (pool_2.token1 == token_out_1)
                 if can_trade_2:
-                    zero_for_one_2 = pool_2.token0 == token_out_1
-                    (token_in_2, token_out_2) = (pool_2.token0, pool_2.token1) if zero_for_one_2 else (pool_2.token1, pool_2.token0)
+                    token_in_2, token_out_2 = (pool_2.token0, pool_2.token1) if pool_2.token0 == token_out_1 else (pool_2.token1, pool_2.token0)
+                    fee_2 = pool_2.fee
                     if token_out_1 != token_in_2:
                         continue
-                    
+
                     for k in range(len(pools)):
                         pool_3 = pools[k]
-                        pools_in_path.append(pool_3.address)
+                        pools_in_path.add(pool_3.address)
                         can_trade_3 = (pool_3.token0 == token_out_2) or (pool_3.token1 == token_out_2)
                         if can_trade_3:
-                            zero_for_one_3 = pool_3.token0 == token_out_2
-                            (token_in_3, token_out_3) = (pool_3.token0, pool_3.token1) if zero_for_one_3 else (pool_3.token1, pool_3.token0)
+                            token_in_3, token_out_3 = (pool_3.token0, pool_3.token1) if pool_3.token0 == token_out_2 else (pool_3.token1, pool_3.token0)
+                            fee_3 = pool_3.fee
                             if token_out_2 != token_in_3:
                                 continue
-                            
+
                             if token_out_3 == token_in:
-                                unique_pool_cnt = len(set(pools_in_path))
-                                
+                                unique_pool_cnt = len(pools_in_path)
+
                                 if unique_pool_cnt < 3:
                                     continue
-                                
+
                                 arb_path = ArbPath(pool_1=pool_1,
                                                    pool_2=pool_2,
                                                    pool_3=pool_3,
-                                                   zero_for_one_1=zero_for_one_1,
-                                                   zero_for_one_2=zero_for_one_2,
-                                                   zero_for_one_3=zero_for_one_3)
+                                                   token_in_1=token_in_1,
+                                                   token_out_1=token_out_1,
+                                                   token_in_2=token_in_2,
+                                                   token_out_2=token_out_2,
+                                                   token_in_3=token_in_3,
+                                                   token_out_3=token_out_3,
+                                                   fee_1=fee_1,
+                                                   fee_2=fee_2,
+                                                   fee_3=fee_3)
                                 paths.append(arb_path)
-                                
+
     logger.info(f'Generated {len(paths)} 3-hop arbitrage paths')
     return paths
 
 
 if __name__ == '__main__':
     from constants import HTTPS_URL
-    from pools import load_all_pools_from_v2
-    
+    from poolsv3 import load_all_pools_from_v3
+
     factory_addresses = [
-        '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',  # Uniswap V2 (Ethereum)
+        '0x1F98431c8aD98523631AE4a59f267346ea31F984',  # Uniswap V3
     ]
     factory_blocks = [
-        10794229,
+        55859483,
     ]
-    
-    pools = load_all_pools_from_v2(HTTPS_URL,
+
+    pools = load_all_pools_from_v3(HTTPS_URL,
                                    factory_addresses,
                                    factory_blocks,
                                    50000)
-    
+
     token_in = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'  # WETH
     paths = generate_triangular_paths(pools, token_in)
     # logger.info(paths)
